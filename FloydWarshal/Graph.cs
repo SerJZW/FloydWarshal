9ad8b3727ca655389b4e423c1d117c4fba9df822
SerJZW <112739060+SerJZW@users.noreply.github.com>
2023-04-12 11:15:10 +0700

    Добавьте файлы проекта.

---
+++ FloydWarshal/Graph.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloydWarshal
+{
+
+    class Graph
+    {
+        private int V; // Количество вершин
+        private int[,] dist; // Матрица расстояний
+
+        public Graph(int vertices)
+        {
+            V = vertices;
+            dist = new int[V, V];
+
+            // Инициализация матрицы расстояний бесконечными значениями
+            for (int i = 0; i < V; i++)
+            {
+                for (int j = 0; j < V; j++)
+                {
+                    dist[i, j] = int.MaxValue;
+                }
+            }
+        }
+
+        // Добавление ребра между вершинами u и v с весом weight
+        public void AddEdge(int u, int v, int weight)
+        {
+            dist[u, v] = weight;
+        }
+
+        // Вычисление кратчайших расстояний между всеми парами вершин
+        public void FloydWarshall()
+        {
+            // Обновляем матрицу расстояний, проходя по всем вершинам
+            for (int k = 0; k < V; k++)
+            {
+                for (int i = 0; i < V; i++)
+                {
+                    for (int j = 0; j < V; j++)
+                    {
+                        // Если путь из вершины i в вершину j через вершину k
+                        // является более коротким, чем текущий путь из i в j,
+                        // то обновляем значение в матрице расстояний
+                        if (dist[i, k] != int.MaxValue && dist[k, j] != int.MaxValue && dist[i, k] + dist[k, j] < dist[i, j])
+                        {
+                            dist[i, j] = dist[i, k] + dist[k, j];
+                        }
+                    }
+                }
+            }
+        }
+
+        // Вывод матрицы кратчайших расстояний
+        public void PrintShortestDistances()
+        {
+            Console.WriteLine("Матрица кратчайших расстояний:");
+            for (int i = 0; i < V; i++)
+            {
+                for (int j = 0; j < V; j++)
+                {
+                    if (dist[i, j] == int.MaxValue)
+                    {
+                        Console.Write("Кротчайший ");
+                    }
+                    else
+                    {
+                        Console.Write(dist[i, j] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ FloydWarshal/Program.cs
@@ -0,0 +1,19 @@
+﻿using FloydWarshal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создание графа
+        Graph graph = new Graph(4);
+        graph.AddEdge(0, 1, 2);
+        graph.AddEdge(0, 2, 6);
+        graph.AddEdge(1, 2, 3);
+        graph.AddEdge(1, 3, 8);
+        graph.AddEdge(2, 3, 1);
+
+        // Вычисление и вывод кратчайших расстояний
+        graph.FloydWarshall();
+        graph.PrintShortestDistances();
+    }
+}
